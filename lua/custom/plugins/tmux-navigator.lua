b651b23984cb4d2649b03aaec9fa9b7582124f38
Curtis Allen <callen@slack-corp.com>
2025-03-25 15:18:44 -0600

    add tmux navigator and vim surround

---
+++ lua/custom/plugins/tmux-navigator.lua
@@ -0,0 +1,23 @@
+-- Make sure you do the proper tmux config for this plugin to work
+-- https://github.com/christoomey/vim-tmux-navigator
+-- which means add
+-- set -g @plugin 'christoomey/vim-tmux-navigator'
+-- to .tmux.conf.local if using oh my tmux
+return {
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+    'TmuxNavigatorProcessList',
+  },
+  keys = {
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+  },
+}
